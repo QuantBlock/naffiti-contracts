3238d5275fbead61cdda9cc52a5684e5bc83ad41
tonyconv <tony@conv.finance>
2022-01-07 00:20:53 +0800

    Change nao token address and startBlock for staking pool

--- scripts/constant/index.ts
+++ scripts/constant/index.ts
@@ -1,7 +1,7 @@
 export const NAFFITI = {
   mainnet: "",
   devchain: "0x90E4aA17f8Dd71BEb83485b084d276205a02b83C",
-  rinkeby: "0x7d8fb3ED16a7135a60A49cad3807F10d13aAD0Ce"
+  rinkeby: "0x4884927BA594998B6ac8E51E7FFA242408Fe1101"
 };
 
 export const TOKEN_ESCROW = {
